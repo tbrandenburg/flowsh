c5a57ecc135c92276950669a712a02745e26f017
Tom Brandenburg <t_bh@gmx.de>
2026-06-27 16:20:32 +0200

    chore: bump version to 0.8.3

--- src/flowsh_cli/__init__.py
+++ src/flowsh_cli/__init__.py
@@ -3,7 +3,7 @@
 from flowsh_cli.models import Workflow, WorkflowParseError, parse_workflows
 from flowsh_cli.render import harness_path, render_harness
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 __all__ = [
     "Workflow",
